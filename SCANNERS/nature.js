c9862c765167eda5a19322d6aacd7f6de91d26a3
morpheous <cerbus.collin@gmail.com>
2017-12-16 22:29:13 -0800

    27

--- SCANNERS/nature.js
+++ SCANNERS/nature.js
@@ -6,8 +6,9 @@ const cheerio = require( "cheerio" );
 // wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 // sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 // sudo apt-get update
-// apt-get install -y google-chrome-unstable
+// sudo apt-get install -y google-chrome-unstable
 // sudo apt-get install libx11-xcb-dev
+// sudo apt-get install libgconf-2-4
 const puppeteer = require( "puppeteer" );
 
 
